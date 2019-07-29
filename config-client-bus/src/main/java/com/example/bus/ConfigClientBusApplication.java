1fd39a5327959812bf5a4215a38d5df673c194b5
ZCsir <zhengchen_12@126.com>
2019-07-29 23:04:09 +0800

    bus

---
+++ config-client-bus/src/main/java/com/example/bus/ConfigClientBusApplication.java
@@ -0,0 +1,24 @@
+package com.example.bus;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ConfigClientBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientBusApplication.class, args);
+    }
+
+
+}
